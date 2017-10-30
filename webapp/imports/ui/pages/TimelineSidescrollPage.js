c46fc2d80108003e314715c7e52f4fec03cc58ba
Abigail Watson <awatson1978@gmail.com>
2017-10-30 13:49:07 -0500

    sync

--- webapp/imports/ui/pages/TimelineSidescrollPage.js
+++ webapp/imports/ui/pages/TimelineSidescrollPage.js
@@ -1,7 +1,30 @@
 import { AboutAppCard } from '/imports/ui/components/AboutAppCard';
 import { GlassCard } from '/imports/ui/components/GlassCard';
 import React from 'react';
+// import Timeline from 'react-visjs-timeline';
 import { VerticalCanvas } from '/imports/ui/components/VerticalCanvas';
+
+// http://visjs.org/docs/timeline/#Configuration_Options
+const options = {
+  width: '100%',
+  height: '60px',
+  stack: false,
+  showMajorLabels: true,
+  showCurrentTime: true,
+  zoomMin: 1000000,
+  type: 'background',
+  format: {
+    minorLabels: {
+      minute: 'h:mma',
+      hour: 'ha'
+    }
+  }
+}
+const items = [{
+  start: new Date(2010, 7, 15),
+  end: new Date(2010, 8, 2),  // end is optional
+  content: 'Trajectory A',
+}]
 
 export class TimelineSidescrollPage extends React.Component {
   constructor(props) {
@@ -12,9 +35,7 @@ export class TimelineSidescrollPage extends React.Component {
     return(
       <div id="aboutPage">
         <VerticalCanvas >
-          <GlassCard height='auto' width='768px'>
-            Timeline
-          </GlassCard>
+          {/* <Timeline options={options} items={items} /> */}
         </VerticalCanvas>
       </div>
     );
