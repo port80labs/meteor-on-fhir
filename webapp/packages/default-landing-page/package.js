87a9744ea797930e0038510d6d09930d9876a5eb
Abigail Watson <awatson1978@gmail.com>
2018-01-23 22:25:07 -0600

    clinical:default-landing-page

--- webapp/packages/default-landing-page/package.js
+++ webapp/packages/default-landing-page/package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'clinical:plugin-default-landing-page',
+  name: 'clinical:default-landing-page',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: '',
